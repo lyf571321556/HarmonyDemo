ea01fa435879e80560f7e9385f55e368b2391671
Liuyanfeng <lyf571321556@163.com>
2023-11-15 11:39:05 +0800

    add: 通过AbilityStage容器启动Hap，对Module进行初始化操作，跨Ability事件回调，全局配置监听（语言，主题色等），内存变更

---
+++ entry/src/main/ets/abilitystage/MyAbilityStage.ts
@@ -0,0 +1,22 @@
+import AbilityStage from '@ohos.app.ability.AbilityStage';
+import hilog from '@ohos.hilog';
+
+export default class MyAbilityStage extends AbilityStage{
+
+  onCreate(){
+    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onCreate")
+  }
+
+  onConfigurationUpdate(newConfig){
+    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onConfigurationUpdate")
+  }
+
+
+  onMemoryLevel(level){
+    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onMemoryLevel")
+  }
+  onAcceptWant(want){
+    // 仅specified模式下触发
+    return "MyAbilityStage";
+  }
+}
