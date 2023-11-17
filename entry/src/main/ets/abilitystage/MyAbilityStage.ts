c0a9f5d8bef9e2397418999221699eba01874c2c
Liuyanfeng <lyf571321556@163.com>
2023-11-17 18:29:50 +0800

    add：容器组件的使用

--- entry/src/main/ets/abilitystage/MyAbilityStage.ts
+++ entry/src/main/ets/abilitystage/MyAbilityStage.ts
@@ -1,21 +1,21 @@
 import AbilityStage from '@ohos.app.ability.AbilityStage';
 import hilog from '@ohos.hilog';
 
-export default class MyAbilityStage extends AbilityStage{
-
-  onCreate(){
-    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onCreate")
+export default class MyAbilityStage extends AbilityStage {
+  onCreate() {
+    hilog.info(0x0001, "MyAbilityStage", "MyAbilityStage onCreate")
   }
 
-  onConfigurationUpdate(newConfig){
-    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onConfigurationUpdate")
+  onConfigurationUpdate(newConfig) {
+    hilog.info(0x0001, "MyAbilityStage", "MyAbilityStage onConfigurationUpdate")
   }
 
 
-  onMemoryLevel(level){
-    hilog.info(0x0001,"MyAbilityStage","MyAbilityStage onMemoryLevel")
+  onMemoryLevel(level) {
+    hilog.info(0x0001, "MyAbilityStage", "MyAbilityStage onMemoryLevel")
   }
-  onAcceptWant(want){
+
+  onAcceptWant(want) {
     // 仅specified模式下触发
     return "MyAbilityStage";
   }
